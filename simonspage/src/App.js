09a2edc15ee67c7daf278cbd34d1e280ee4a29e6
SimonBraun1508 <81743075+SimonBraun1508@users.noreply.github.com>
2021-07-10 19:05:45 +0200

    react

---
+++ simonspage/src/App.js
@@ -0,0 +1,12 @@
+import MainView from './components/MainView'
+
+
+function App() {
+  return (
+    <div >
+      <MainView/>
+    </div>
+  );
+}
+
+export default App;
---
+++ simonspage/src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Main extends React.Component {
+    render() {
+      return (
+  
+        <main>
+          <div><h2>Produktübersicht</h2>
+          </div>
+          <hr />
+        </main>
+      );
+    }
+  };
+  
+  export default Main;
