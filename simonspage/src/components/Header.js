0d8b55a143c9cb1cfacf4b8ccf8a87ca60030c6a
SimonBraun1508 <81743075+SimonBraun1508@users.noreply.github.com>
2021-07-23 18:25:19 +0200

    123

--- simonspage/src/components/Header.js
+++ simonspage/src/components/Header.js
@@ -5,14 +5,16 @@ import logoplaceholder from './img/Logoplaceholder.png'
 export default function Header() {
     return (
         <div className="header">
-            <div class="row myrow">
+            {/* <div class="row myrow">
                 <div class='col mycol'>12334</div>
                 <div class='col mycol'>232241</div>
                 <div class='col mycol'>1</div>
                 <div class='col mycol'>1</div>
-            </div>
+            </div> */}
             <div class="row myrow">
                 <div class='col mycol'><img className="logo" src={logoplaceholder} alt="Logo"></img> </div>
+                <div class='col mycol2'>meaningful text</div>
+                <div class='col mycol2'>meaningful text</div>
                 <div class='col mycol2'>meaningful text</div>
                 {/* <div class='col mycol'>1</div> */}
                 {/* <div class='col mycol'>1</div> */}
