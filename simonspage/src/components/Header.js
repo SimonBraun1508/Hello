87e35dc9dbc613e4d3a44cc2a7f7428736b0f626
SimonBraun1508 <81743075+SimonBraun1508@users.noreply.github.com>
2021-07-23 19:27:48 +0200

    Update Header.js

--- simonspage/src/components/Header.js
+++ simonspage/src/components/Header.js
@@ -12,7 +12,7 @@ export default function Header() {
                 <div class='col mycol'>1</div>
             </div> */}
             <div class="row myrow">
-                <div class='col mycol'><img className="logo" src={logoplaceholder} alt="Logo"></img> </div>
+                <div class='col mycol2'><img className="logo" src={logoplaceholder} alt="Logo"></img> </div>
                 <div class='col mycol2'>meaningful text</div>
                 <div class='col mycol2'>meaningful text</div>
                 <div class='col mycol2'>meaningful text</div>
