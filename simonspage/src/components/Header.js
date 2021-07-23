e78f6add6c99cd6be9ba05a5d13bbbbca65c771d
SimonBraun1508 <81743075+SimonBraun1508@users.noreply.github.com>
2021-07-23 17:32:01 +0200

    1212

--- simonspage/src/components/Header.js
+++ simonspage/src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import './Header.css'
+import logoplaceholder from './img/Logoplaceholder.png'
 
 export default function Header() {
     return (
-        <div>
+        <div className="header">
             <div class="row myrow">
                 <div class='col mycol'>12334</div>
                 <div class='col mycol'>232241</div>
@@ -11,8 +12,8 @@ export default function Header() {
                 <div class='col mycol'>1</div>
             </div>
             <div class="row myrow">
-                <div class='col mycol'>12334</div>
-                <div class='col mycol'>232241</div>
+                <div class='col mycol'><img className="logo" src={logoplaceholder} alt="Logo"></img> </div>
+                <div class='col mycol2'>meaningful text</div>
                 {/* <div class='col mycol'>1</div> */}
                 {/* <div class='col mycol'>1</div> */}
             </div>
