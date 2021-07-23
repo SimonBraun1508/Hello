be0a6d6ba414bed2272ed090c9218631befa43b2
SimonBraun1508 <81743075+SimonBraun1508@users.noreply.github.com>
2021-07-23 16:48:41 +0200

    Update Header.js

--- simonspage/src/components/Header.js
+++ simonspage/src/components/Header.js
@@ -5,8 +5,8 @@ export default function Header() {
     return (
         <div>
             <div class="row myrow">
-                <div class='col mycol'>1</div>
-                <div class='col mycol'>1</div>
+                <div class='col mycol'>12334</div>
+                <div class='col mycol'>232241</div>
                 <div class='col mycol'>1</div>
                 <div class='col mycol'>1</div>
             </div>
